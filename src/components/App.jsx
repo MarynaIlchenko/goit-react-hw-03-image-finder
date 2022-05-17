7164104fe99d99248a7546b61975c09a34a965d2
MarynaIlchenko <debovamail@gmail.com>
2022-05-17 20:30:46 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,101 @@
 import { Component } from 'react';
-import { Searchbar } from './Searchbar/Searchbar';
+import { Searchbar } from './Searchbar';
+import { ApiService } from './services/api';
+import { ImageGallery } from './ImageGallery';
+import { Modal } from './Modal';
+import { Button } from './Button';
+import { Loader } from './Loader';
+
+const apiService = new ApiService();
 
 export class App extends Component {
   state = {
-    loading: false,
     imageArr: [],
-    largeImageURL: '',
+    query: '',
+    error: null,
+    isLoading: false,
+    showModal: false,
+    largeImgURL: '',
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { query } = this.state;
+    apiService.query = query;
+
+    if (prevState.query !== query) {
+      this.onFetchImage();
+    }
+  }
+
+  onSearch = newQuery => {
+    this.setState({
+      query: newQuery,
+    });
+  };
+
+  onToggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
+  onClickImg = event => {
+    this.setState({
+      largeImgURL: this.state.imageArr.find(
+        img => img.webformatURL === event.target.src
+      ).largeImageURL,
+    });
+  };
+
+  onFetchImage = async () => {
+    this.setState({ isLoading: true });
+
+    try {
+      const imageArr = await apiService.fetchImage();
+      this.setState({ imageArr });
+    } catch (error) {
+      this.setState({ error });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  onLoadMore = async () => {
+    this.setState({ isLoading: true });
+
+    try {
+      const imageArr = await apiService.fetchImage();
+      this.setState(prevState => ({
+        imageArr: [...prevState.imageArr, ...imageArr],
+      }));
+    } catch (error) {
+      this.setState({ error });
+    } finally {
+      this.setState({ isLoading: false });
+    }
   };
 
   render() {
+    const { isLoading, imageArr, showModal, largeImgURL } = this.state;
     return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Searchbar />
-      </div>
+      <>
+        <Searchbar onSubmit={this.onSearch} />
+
+        {
+          <ImageGallery
+            onClickImg={this.onClickImg}
+            images={imageArr}
+            onToggleModal={this.onToggleModal}
+          />
+        }
+        {showModal && (
+          <Modal onToggleModal={this.onToggleModal} img={largeImgURL} />
+        )}
+        {isLoading && <Loader />}
+        {imageArr.length >= 12 && (
+          <Button onLoadMore={this.onLoadMore} loading={isLoading} />
+        )}
+      </>
     );
   }
 }
---
+++ src/components/Button/Button.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import style from './Button.module.css';
+
+export const Button = ({ onLoadMore, loading }) => {
+  return (
+    <div className={style.ButtonBlock}>
+      {!loading && (
+        <button onClick={onLoadMore} className={style.Button} type="button">
+          Load More
+        </button>
+      )}
+    </div>
+  );
+};
+
+Button.propTypes = {
+  onLoadMore: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
